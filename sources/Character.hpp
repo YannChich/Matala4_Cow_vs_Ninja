79adefa475ec668192ce683b07b5f319c989e523
YannChich <yanoukaxbox@gmail.com>
2023-05-17 04:11:09 +0300

    Cowboy hpp/cpp

--- sources/Character.hpp
+++ sources/Character.hpp
@@ -24,18 +24,18 @@ public:
 // Differents function that every type of Character going to share
 
 /*
-This function return True if the Character is alive or False (cheking the heal of the character)
+-This function return True if the Character is alive or False (cheking the heal of the character)
 */
     bool isAlive() const;
 
 /*
-This function going to return the distance between two characters on the game
-The function receives as parameter a ptr of a Character
+-This function going to return the distance between two characters on the game
+-The function receives as parameter a ptr of a Character
 */
     double distance(Character* character) const;
 
 /*
-The function going to reduce the health of a Character basing on the damage of the enemy
+-The function going to reduce the health of a Character basing on the damage of the enemy
 */
     void hit(int damage);
 
@@ -50,10 +50,10 @@ The function going to reduce the health of a Character basing on the damage of t
     void setLocation(Point& new_Location);
 
 /*
-This function print going to print the details of the Character like : Name , damage , health
-If the character is dead going to print the name in parenthese
-Before each prints , will see the Char N(Ninja) and C(Cowboy)
-The function going to be virtual because we got differents Characters and differents details for each of them
+-This function print going to print the details of the Character like : Name , damage , health
+-If the character is dead going to print the name in parenthese
+-Before each prints , will see the Char N(Ninja) and C(Cowboy)
+-The function going to be virtual because we got differents Characters and differents details for each of them
 */
     virtual string print() const = 0;
     };
--- sources/Point.hpp
+++ sources/Point.hpp
@@ -22,21 +22,21 @@ public :
     Point(double graph_x,double graph_y);
 
 /* Function to calculate the distance 
-Ex : we got (2,3) and (4,8) to calculate the distance we need to use Pythagore : Distance² = (4-2)² + (8-3)² 
+-Ex : we got (2,3) and (4,8) to calculate the distance we need to use Pythagore : Distance² = (4-2)² + (8-3)² 
                                                                                            = 29   ---> Distance = sqrt(29)
 */
     double distance(const Point& other) const;
 
 /* Function to print the point
-we need to return an object of ostringstream to use the function like in the demo.
-Ex : we got P1 if we print him it's look like : (x,y)
+-we need to return an object of ostringstream to use the function like in the demo.
+-Ex : we got P1 if we print him it's look like : (x,y)
 */
     string print() const;
 
 /* Function moveTowards return the closest point to the dest Point
-The function got 3 arguments : the distance , the source and the desitination
-The function going to be static because we dont need instance of the class Point to use it
-Logic : 
+-The function got 3 arguments : the distance , the source and the desitination
+-The function going to be static because we dont need instance of the class Point to use it
+-Logic : 
 */
     static Point moveTowards(const Point& source,const Point& destination,double distance);
 
